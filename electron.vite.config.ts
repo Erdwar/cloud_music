c9227ed701d9b8739218af81b0f9820f4e74f7e6
saber12139 <1079904159@qq.com>
2024-01-19 13:06:41 +0800

    第二次

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()]
@@ -12,9 +14,32 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@comp': resolve('src/renderer/src/components'),
+        '@assets': resolve('src/renderer/assets')
       }
     },
-    plugins: [vue()]
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ],
+    css: {
+      preprocessorOptions: {
+        less: {
+          // math: 'always',
+          // additionalData: `@import "./src/renderer/src/assets/css/styles.less";`,
+          globalVars: {
+            'primary-color': '#1DA57A'
+          },
+          modifyVars: {},
+          javascriptEnabled: true
+        }
+      }
+    }
   }
 })
